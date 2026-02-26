b098a0e24b0299067e11662e1e0644ee87990bb3
Ondrej Klika <o.klika@proton.me>
2026-02-26 13:37:51 +0100

    init project

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import './app.css';
+</script>
+
+<main class="mx-auto">
+	<header>
+		<h1>Skybox builder</h1>
+	</header>
+	<article>
+		<p>
+			This is a simple utility tool for generating simpe skybox cubemap textures. If you want to
+			generate a cubemap from panorama photos, you can use <a href="https://github.com/jaxry"
+				>Lucas Crane</a
+			>'s
+			<a href="https://github.com/jaxry/panorama-to-cubemap">panorama-to-cubemap</a>.
+		</p>
+		<p>
+			The main motivation behind this tool was me trying to add a simple gradient skybox to my Bevy
+			project and finding out that the process is a bit more complicated then I expected it to be.
+			And I can also try out Svelte!
+		</p>
+	</article>
+</main>
